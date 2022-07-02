e135039fb0a7771849fccb4dbd55617cd139cf59
Kailash Bharat <kailashbb12@hotmail.com>
2022-07-02 13:50:03 +0200

    Added some new files

---
+++ test/test.js
@@ -0,0 +1,23 @@
+const assert = require("assert");
+
+function add(x, y) {
+  return x + y;
+}
+
+describe("Testing bizz", () => {
+  it("should test the add function", () => {
+    assert.equal(add(2, 2), 4);
+  });
+  it("should test the add function", () => {
+    assert.equal(add(2, 2), 4);
+  });
+});
+
+describe("Testing bizz", () => {
+  it("should test the add function", () => {
+    assert.equal(add(2, 2), 4);
+  });
+  it("should test the add function", () => {
+    assert.equal(add(2, 2), 4);
+  });
+});
