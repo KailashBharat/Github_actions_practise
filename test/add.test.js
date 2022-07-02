5030fde2215b67e3275ac181ae58ba6b24c9f3a2
Kailash Bharat <kailashbb12@hotmail.com>
2022-07-02 16:05:07 +0200

    Passing tests

--- test/add.test.js
+++ test/add.test.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const {add, sub} = require("./index.js")
+const {add, sub} = require("../index.js")
 
 describe("Testing bizz", () => {
   it("should test the add function", () => {
@@ -24,6 +24,6 @@ describe("Testing bizz", () => {
     assert.equal(add(2, 2), 4);
   });
   it("should test the add function", () => {
-    assert.equal(add(3, 2), 4);
+    assert.equal(add(2, 2), 4);
   });
 });
