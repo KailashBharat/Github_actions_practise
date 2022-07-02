23aaef8f5d785db7043b127c37dd9cd334f94d0e
Kailash Bharat <kailashbb12@hotmail.com>
2022-07-02 15:39:09 +0200

    Some more file changes

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function add(x, y) {
 function sub(x, y) {
   return x - y;
 }
+
+module.exports = {
+  add,
+  sub,
+};
--- test/add.test.js
+++ test/add.test.js
@@ -1,11 +1,21 @@
 const assert = require("assert");
+const {add, sub} = require("./index.js")
 
 describe("Testing bizz", () => {
   it("should test the add function", () => {
     assert.equal(add(2, 2), 4);
   });
+  it("should test the sub function", () => {
+    assert.equal(sub(2, 2), 0);
+  });
+});
+
+describe("Testing bizz", () => {
   it("should test the add function", () => {
     assert.equal(add(2, 2), 4);
+  });
+  it("should test the sub function", () => {
+    assert.equal(sub(2, 2), 0);
   });
 });
 
